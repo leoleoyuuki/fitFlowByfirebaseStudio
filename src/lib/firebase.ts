aee7e57ee1d32765c8e554a5e7594ff803aa4e65
Leonardo Yuuki Nakazone <leoyuuki2005@gmail.com>
2025-06-04 18:47:47 +0000

    Add enviroment variable to firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore"; // Added Firestore import
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDScdZ1qIT3I9sGMOtvnGYrMAnQ-W-02ZM",
-  authDomain: "saas-fitness1.firebaseapp.com",
-  projectId: "saas-fitness1",
-  storageBucket: "saas-fitness1.firebasestorage.app",
-  messagingSenderId: "593509228481",
-  appId: "1:593509228481:web:c01b1664df9a0fd8b19940"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
