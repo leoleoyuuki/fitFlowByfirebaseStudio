f5843c90f83ebba1830224ab7838eecf2f8d0866
Leonardo Yuuki Nakazone <leoyuuki2005@gmail.com>
2025-06-30 05:01:47 +0000

    PRoduto teste

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -71,7 +71,7 @@ export const MOCK_SUBSCRIPTION_PLANS: SubscriptionPlan[] = [
       "Aumente a retenção e a percepção de valor dos seus alunos",
       "Suporte prioritário para sua equipe"
     ], 
-    stripePriceId: "price_1RfZWYKOlX31rMUJXmeVodiK"
+    stripePriceId: "price_1RfZpmKOlX31rMUJWypRXgee"
   },
 ];
 
